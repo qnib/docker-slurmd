bf7af8e2f2be545407fae6f57e3fb8abcc431542
Christian Kniep <christian@qnib.org>
2015-04-07 10:00:38 +0200

    jobscripts put into slurmd

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ MAINTAINER "Christian Kniep <christian@qnib.org>"
 
 ADD etc/supervisord.d/slurmd.ini /etc/supervisord.d/
 ADD etc/consul.d/check_slurmd.json /etc/consul.d/
+ADD opt/qnib/jobscripts/bin/ /opt/qnib/jobscripts/bin/
---
+++ opt/qnib/jobscripts/bin/ping_pong.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=PingPong
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --ntasks-per-node=1
+
+mkdir -p ${HOME}/slurm/${SLURM_JOBID}/
+cd ${HOME}/slurm/${SLURM_JOBID}/
+DOMAIN=node.consul
+BW_LIMIT=3000
+for node in $(echo ${SLURM_NODELIST}|sed -e 's/,/ /g');do
+    if [ ${node} != ${SLURMD_NODENAME} ];then
+        logger --tag slurm_${SLURM_JOBID} "fallocate -l 60M /tmp/test.dd"
+        srun --exclusive -n1 fallocate -l 60M /tmp/test.dd
+        sleep 15
+        logger --tag slurm_${SLURM_JOBID} "rsync file with bwlimit=${BW_LIMIT} to ${node}.${DOMAIN}:${HOME}/slurm/${SLURM_JOBID}/"
+        srun --exclusive -n1 rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:${HOME}/slurm/${SLURM_JOBID}/
+        logger --tag slurm_${SLURM_JOBID} "Delete local file and file within ${HOME}/slurm/${SLURM_JOBID}/"
+        srun --exclusive -n1 ${HOME}/slurm/${SLURM_JOBID}/test.dd /tmp/test.dd
+        sleep 15
+    fi
+done
