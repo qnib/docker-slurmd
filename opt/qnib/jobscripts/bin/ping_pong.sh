156fd695779e2ab336025f7ee93fbb67f3f4d3ec
Christian Kniep <christian@qnib.org>
2015-04-24 14:56:43 +0200

    make pingpong more robust

--- opt/qnib/jobscripts/bin/ping_pong.sh
+++ opt/qnib/jobscripts/bin/ping_pong.sh
@@ -11,16 +11,16 @@ DOMAIN=node.consul
 BW_LIMIT=3000
 for node in $(echo ${SLURM_NODELIST}|sed -e 's/,/ /g');do
     if [ ${node} != ${SLURMD_NODENAME} ];then
-        logger --tag slurm_${SLURM_JOBID} "fallocate -l 60M /tmp/test.dd"
-        srun --exclusive -n1 fallocate -l 60M /tmp/test.dd
+        logger --tag slurm_${SLURM_JOBID} "fallocate -l 60M /tmp/test_${SLURM_JOBID}.dd"
+        srun --exclusive -n1 fallocate -l 60M /tmp/test_${SLURM_JOBID}.dd
         sleep 15
         logger --tag slurm_${SLURM_JOBID} "mkdir -p /scratch/${SLURM_JOBID}/"
         mkdir -p /scratch/${SLURM_JOBID}/
         sleep 15
-        logger --tag slurm_${SLURM_JOBID} "rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/"
-        srun --exclusive -n1 rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/
+        logger --tag slurm_${SLURM_JOBID} "rsync --bwlimit=${BW_LIMIT} -aP /tmp/test_${SLURM_JOBID}.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/"
+        srun --exclusive -n1 rsync --bwlimit=${BW_LIMIT} -aP /tmp/test_${SLURM_JOBID}.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/
         logger --tag slurm_${SLURM_JOBID} "Delete local file and file within /scratch/${SLURM_JOBID}/"
-        srun --exclusive -n1 rm -f /scratch/${SLURM_JOBID}/test.dd /tmp/test.dd
+        srun --exclusive -n1 rm -f /scratch/${SLURM_JOBID}/test_${SLURM_JOBID}.dd /tmp/test_${SLURM_JOBID}.dd
         sleep 15
     fi
 done
