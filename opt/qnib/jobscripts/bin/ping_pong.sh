31b005be8d7f5daeb0a36afd4ff180a1eec04b0c
Christian Kniep <christian@qnib.org>
2015-04-24 09:22:04 +0200

    use /scratch instead of /home

--- opt/qnib/jobscripts/bin/ping_pong.sh
+++ opt/qnib/jobscripts/bin/ping_pong.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=PingPong
+#SBATCH --workdir=/scratch/
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 #SBATCH --ntasks-per-node=1
 
-mkdir -p ${HOME}/slurm/${SLURM_JOBID}/
-cd ${HOME}/slurm/${SLURM_JOBID}/
+mkdir -p /scratch/${SLURM_JOBID}/
+cd /scratch/${SLURM_JOBID}/
 DOMAIN=node.consul
 BW_LIMIT=3000
 for node in $(echo ${SLURM_NODELIST}|sed -e 's/,/ /g');do
@@ -13,13 +14,13 @@ for node in $(echo ${SLURM_NODELIST}|sed -e 's/,/ /g');do
         logger --tag slurm_${SLURM_JOBID} "fallocate -l 60M /tmp/test.dd"
         srun --exclusive -n1 fallocate -l 60M /tmp/test.dd
         sleep 15
-        logger --tag slurm_${SLURM_JOBID} "mkdir -p ${HOME}/slurm/${SLURM_JOBID}/"
-        mkdir -p ${HOME}/slurm/${SLURM_JOBID}/
+        logger --tag slurm_${SLURM_JOBID} "mkdir -p /scratch/${SLURM_JOBID}/"
+        mkdir -p /scratch/${SLURM_JOBID}/
         sleep 15
-        logger --tag slurm_${SLURM_JOBID} "rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:${HOME}/slurm/${SLURM_JOBID}/"
-        srun --exclusive -n1 rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:${HOME}/slurm/${SLURM_JOBID}/
-        logger --tag slurm_${SLURM_JOBID} "Delete local file and file within ${HOME}/slurm/${SLURM_JOBID}/"
-        srun --exclusive -n1 rm -f ${HOME}/slurm/${SLURM_JOBID}/test.dd /tmp/test.dd
+        logger --tag slurm_${SLURM_JOBID} "rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/"
+        srun --exclusive -n1 rsync --bwlimit=${BW_LIMIT} -aP /tmp/test.dd ${node}.${DOMAIN}:/scratch/${SLURM_JOBID}/
+        logger --tag slurm_${SLURM_JOBID} "Delete local file and file within /scratch/${SLURM_JOBID}/"
+        srun --exclusive -n1 rm -f /scratch/${SLURM_JOBID}/test.dd /tmp/test.dd
         sleep 15
     fi
 done
